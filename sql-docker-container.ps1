975190056c8e6c54c0449370856280f1d4bf31cf
Team Tinitiate / Venkata Bhattaram <tinitiate@users.noreply.github.com>
2023-04-22 21:45:34 -0400

    Update sql-docker-container.ps1

--- sql-docker-container.ps1
+++ sql-docker-container.ps1
@@ -4,5 +4,7 @@ git clone https://github.com/tinitiate/mssql-docker.git
 # Navigate to the directory
 cd .\mssql-docker\
 
+docker volume create --name=mssql-data --opt type=none --opt device=$(pwd)/data --opt o=bind
+
 # Run docker-compose
 docker-compose up -d
